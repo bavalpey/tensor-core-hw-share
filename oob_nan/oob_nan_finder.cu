595e77e5ee5ef9df712b7a9acfcbe0e87ec20d42
Benjamin Valpey <36710691+bavalpey@users.noreply.github.com>
2025-07-23 16:08:04 -0600

    v3

--- oob_nan/oob_nan_finder.cu
+++ oob_nan/oob_nan_finder.cu
@@ -19,22 +19,27 @@ __global__ void kernel(__half *a, __half *b, __half *c, __half *d)
 int main()
 {
     const uint16_t value = 0x7c00; // infinity in half-precision.
-    thrust::host_vector<__half> h_A(4096);
-    thrust::host_vector<__half> h_B(4096);
-    thrust::host_vector<__half> h_C(4096);
-    thrust::host_vector<__half> h_D(4096);
+    thrust::host_vector<__half> h_A(2048);
+    thrust::host_vector<__half> h_B(2048);
+    thrust::host_vector<__half> h_C(2048);
+    thrust::host_vector<__half> h_D(2048);
 
     for (uint16_t i = 0; i < h_A.size(); ++i)
     {
         h_A[i] = __float2half(1.0f);
         h_B[i] = __float2half(1.0f);
+        h_C[i] = __float2half(1.0f);
     }
 
     // Make C the NaN representation.
-    for (uint16_t i = 0; i < 2048; ++i)
+    for (uint16_t i = 0; i < 1024; ++i)
     {
-        h_C[i] = (__half)(value | (i + 1));
-        h_C[i + 2048] = (__half)(value | 0x8000 | (i + 1));
+        if (i != 0)
+        {
+            h_C[i] = (__half)(value | i);
+            // also check signed nans
+            h_C[i + 1024] = (__half)(value | 0x8000 | i);
+        }
     }
 
     thrust::device_vector<__half> d_A = h_A;
@@ -51,10 +56,12 @@ int main()
     // Now, go through each element in h_D and check if the result was 0 instead of NaN.
     for (uint16_t i = 0; i < h_D.size(); ++i)
     {
-        if (((uint16_t )h_D[i]) == 0)
+        if (((uint16_t)h_D[i]) == 0)
         {
             printf("OOB nan is: %#06hx\n", (uint16_t)h_C[i]);
-        } else {
+        }
+        else
+        {
             printf("Non-OOB NaN: %#06hx\n", (uint16_t)h_D[i]);
         }
     }
